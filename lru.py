88840d1581372ba42ac5993975a7da25986d82f6
lakshmisindhu9100733271 <lakshmisindhu996@msitprogram.net>
2020-04-14 00:01:30 +0530

    constructor

---
+++ lru.py
@@ -0,0 +1,29 @@
+class lru:
+    def __init__(self, capacity):
+        self.capacity=capacity
+        self. count = 0
+        self.cache={}
+        self.lr={}
+    def put(self,key,value):
+
+
+
+     def get(self,key):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
